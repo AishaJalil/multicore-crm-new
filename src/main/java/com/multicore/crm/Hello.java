aeb73be7b1275f134c3907e839b8f20d9409418d
LaibaMohsin3002 <Laibamohsin17@gmail.com>
2025-11-22 23:38:25 +0500

    Initial commit: Spring Boot MultiCore CRM project

---
+++ src/main/java/com/multicore/crm/Hello.java
@@ -0,0 +1,7 @@
+package com.multicore.crm;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java is working!");
+    }
+}
---
+++ src/test/java/com/multicore/crm/MultiCoreCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multicore.crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiCoreCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
